bcc895a0b0d2386f31d16aa572a3bd6800b428f9
punk8 <punk8sang@gmail.com>
2024-04-13 21:51:40 +0800

    feat: hook contract

--- contracts/core/HookV1.sol
+++ contracts/core/HookV1.sol
@@ -4,27 +4,27 @@ pragma solidity ^0.8.16;
 import "../interfaces/IYexFTOFactory.sol";
 import "./YexFTOPair.sol";
 import "../interfaces/IERC20.sol";
+import "../interfaces/IHook.sol";
 
-contract HookV1 {
+contract HookV1 is IHook {
     address public factory;
-    address public feeToken;
-    // uint256 public feePercent;
 
     mapping(address => mapping(address => uint)) locks;
+    mapping(address => mapping(address => uint256)) lockAmounts;
+    mapping(address => mapping(address => address)) feeTokens;
     mapping(address => mapping(address => address)) providers;
     mapping(address => mapping(address => mapping(address => uint))) fees;
 
-    constructor(address _factory, address _feeToken) {
+    constructor(address _factory) {
         factory = _factory;
-        feeToken = _feeToken; // maybe usdt
-        // feePercent = _feePercent; // like 5 so that we can get 5
     }
 
     function launch(
         address originToken,
         address raisedToken, // maybe usdt
-        string calldata name,
-        string calldata symbol,
+        address feeToken,
+        string calldata launchedTokenName,
+        string calldata launchedTokensymbol,
         uint256 _amount,
         address poolHandler,
         uint256 lockTime,
@@ -49,31 +49,35 @@ contract HookV1 {
         );
 
         address launchedToken = YexFTOPair(pair).launchedToken();
+
         locks[originToken][launchedToken] = block.timestamp + lockTime;
+        lockAmounts[originToken][launchedToken] = _amount;
         providers[originToken][launchedToken] = msg.sender;
+        feeTokens[originToken][launchedToken] = feeToken;
 
         // emit some envent
+        emit Launch(originToken, launchedToken, _amount);
     }
 
     function withdraw(
         address originToken,
         address launchedToken,
-        uint256 _amount
-    ) {
+        uint256 feeAmount
+    ) external override {
         address provider = providers[originToken][launchedToken];
         require(provider == msg.sender, "only provider can withdraw fee.");
         require(
-            _amount <= fees[originToken][launchedToken][provider],
-            "not enough amount."
+            feeAmount <= fees[originToken][launchedToken][provider],
+            "not enough fee."
         );
-        IERC20(originToken).transfer(msg.sender, _amount);
+        IERC20(originToken).transfer(msg.sender, feeAmount);
     }
 
     function unlock(
         address originToken,
         address launchedToken,
         uint256 _amount
-    ) {
+    ) external override {
         // 1. check wether locktime is done and amount is enough.
         require(
             locks[originToken][launchedToken] >= block.timestamp,
@@ -83,10 +87,14 @@ contract HookV1 {
             IERC20(originToken).balanceOf(address(this)) >= _amount,
             "not enough amount."
         );
+        require(
+            lockAmounts[originToken][launchedToken] >= _amount,
+            "not enough amount."
+        );
 
         // 2. receive launchedToken + some fee (like 1usdt)
 
-        uint256 _fee = _calculateFee(_amount);
+        uint256 _fee = _calculateFee(originToken, launchedToken, _amount);
 
         TransferHelper.safeTransferFrom(
             launchedToken,
@@ -107,9 +115,14 @@ contract HookV1 {
 
         // 3. send originToken
         IERC20(originToken).transfer(msg.sender, _amount);
+        lockAmounts[originToken][launchedToken] -= _amount;
     }
 
-    function _calculateFee(uint256 amount) internal view {
+    function calculateFee(
+        address originToken,
+        address launchedToken,
+        uint256 amount
+    ) external view override {
         return 10 ** 18;
     }
 }
